b96c4c636e71f665582a113c0c691052fac45969
Shun <shun12baseball@gmail.com>
2021-11-14 18:28:30 +0900

    feat: add stimulus tutorial.

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["init", "name", "output"]
+
   connect() {
-    this.element.textContent = "Hello World!"
+    this.initTarget.textContent = "Hello World!"
+  }
+
+  greet() {
+    this.outputTarget.textContent =
+      `Hello, ${this.nameTarget.value}`
   }
 }
